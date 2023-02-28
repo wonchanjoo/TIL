f0f7f084c07e121e01b8afcf7df8cb3a65db4675
wonchanjoo <wcj0911@naver.com>
2023-02-28 11:51:35 +0900

    update BackJoon2018 use two pointer

--- BaekJoon/BaekJoon2018/src/Main.java
+++ BaekJoon/BaekJoon2018/src/Main.java
@@ -1,13 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-        int startIndex, endIndex = 1;
-        int count = 1;
+        int start = 1, end = 1;
+        int sum = 1, count = 0;
 
+        while(start <= end) {
+            if(sum == N) {
+                count++;
+            }
 
+            if(sum < N) {
+                end++;
+                sum += end;
+            } else {
+                sum -= start;
+                start++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
