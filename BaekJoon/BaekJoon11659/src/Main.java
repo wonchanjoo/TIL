ef70ff7db3cd40ce2c380543202bbfb32c2f1579
wonchanjoo <wcj0911@naver.com>
2023-03-03 23:52:15 +0900

    delete comments

--- BaekJoon/BaekJoon11659/src/Main.java
+++ BaekJoon/BaekJoon11659/src/Main.java
@@ -3,12 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/*
-수 N개가 주어졌을 때 i 번째 수에서 j 번째 수까지의 합을 구하는 프로그램을 작성하시오
-입력: 1번째 줄에 수의 개수 N(1 <= N <= 100,000), 합을 구해야 하는 횟수M(1 <= M <= 100,000),
-     2번째 줄에 N개의 수가 주어진다. 각 수는 1,000보다 작거나 같은 자연수다. 3번째 줄부터는 M개의 줄에 합을 구해야하는 구간 i와 j가 주어진다.
-출력: 총 M개의 줄에 입력으로 주어진 i번째 수에서 j번째 수까지의 합을 출력한다.
- */
 public class Main {
 
     public static void main(String[] args) throws IOException {
