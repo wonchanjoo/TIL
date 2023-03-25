3c2fcaa55145b4ea54fe3d86d4002b5f3de9626a
wonchanjoo <wcj0911@naver.com>
2023-03-25 23:56:52 +0900

    add Programmers 행렬의 덧셈

---
+++ Programmers/Lv.1/행렬의 덧셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        for(int r = 0; r < arr1.length; r++)
+            for(int c = 0; c < arr1[r].length; c++)
+                arr1[r][c] = arr1[r][c] + arr2[r][c];
+                
+        return arr1;
+    }
+}
