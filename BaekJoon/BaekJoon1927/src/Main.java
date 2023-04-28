e6dde10b88b8fad3e2e6053bfabb7734d45790da
wonchanjoo <wcj0911@naver.com>
2023-04-28 21:16:34 +0900

    [BaekJoon] 1927 최소 힙

---
+++ BaekJoon/BaekJoon1927/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x > 0) {
+                queue.offer(x);
+            } else {
+                if(queue.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(queue.poll()).append('\n');
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}
