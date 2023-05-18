af7f8c3ad80131d22ee955dcf99cb09a41f2205e
wonchanjoo <wcj0911@naver.com>
2023-05-18 19:51:43 +0900

    [BaekJoon] 2178 미로 탐색

--- BaekJoon/BaekJoon2178/src/Main.java
+++ BaekJoon/BaekJoon2178/src/Main.java
@@ -1,38 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static int N;
+    static int M;
     static boolean visited[][];
-    static int maze[][];
-    static int dirY[] = {-1, 1, 0, 0};
-    static int dirX[] = {0, 0, -1, 1};
-    static Queue<Node> queue = new LinkedList<>();
-
-    static int now
-
-    static class Node {
-        int x;
-        int y;
-
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+    static int map[][];
+    static int count[][];
+    static int dirR[] = {1, -1, 0, 0}; // 동 서 남 북
+    static int dirC[] = {0, 0, 1, -1}; // 동 서 남 북
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N][M];
         map = new int[N][M];
-        dir = new int[N][M];
+        count = new int[N][M];
         for(int r = 0; r < N; r++) {
             String s = br.readLine();
             for(int c = 0; c < M; c++) {
@@ -40,20 +30,31 @@ public class Main {
             }
         }
 
-        countDir(0, 0);
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                System.out.print(dir[i][j] + " ");
+        countPath();
+        System.out.println(count[N - 1][M - 1]);
+    }
+
+    private static void countPath() {
+        Queue<Integer[]> q = new ArrayDeque<>();
+        count[0][0] = 1;
+        visited[0][0] = true;
+        q.offer(new Integer[]{0, 0});
+
+        while(!q.isEmpty()) {
+            Integer[] n = q.poll();
+            for(int i = 0; i < 4; i++) { // 동서남북 중 아직 방문하지 않은 노드
+                int newR = n[0] + dirR[i];
+                int newC = n[1] + dirC[i];
+                if(isRight(newR, newC) && map[newR][newC] == 1 && !visited[newR][newC]) {
+                    q.offer(new Integer[]{newR, newC});
+                    count[newR][newC] = count[n[0]][n[1]] + 1;
+                    visited[newR][newC] = true;
+                }
             }
-            System.out.println();
         }
     }
 
-    private static void countDir(int x, int y) {
-        dir[x][y]++;
-    }
-
-    private static int getShortestPathCount() {
-        return 0;
+    private static boolean isRight(int r, int c) {
+        return (r >= 0 && c >= 0 && r < N && c < M);
     }
 }
