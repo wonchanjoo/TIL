9df96a81c757ddb01eb4e586d75fbd1d6f658b0c
wonchanjoo <wcj0911@naver.com>
2023-07-07 12:41:25 +0900

    [BaekJoon] 14502 연구소 (incomplete)

---
+++ BaekJoon/BaekJoon14502/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++)
+                map[i][j] = Integer.parseInt(st.nextToken());
+        } // 입력 끝
+    }
+}
