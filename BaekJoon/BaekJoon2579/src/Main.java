2eb8ea4bef116541ca57527cdd863040f743c316
wonchanjoo <wcj0911@naver.com>
2023-07-19 21:13:13 +0900

    [Programmers] 숫자 변환하기

--- BaekJoon/BaekJoon2579/src/Main.java
+++ BaekJoon/BaekJoon2579/src/Main.java
@@ -5,37 +5,36 @@ import java.io.InputStreamReader;
 public class Main {
 
     static int[] stairs;
-    static int[] before;
+    static int[] D;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         stairs = new int[N];
-        before = new int[N];
+        D = new int[N];
         for(int i = 0; i < N; i++) {
             stairs[i] = Integer.parseInt(br.readLine());
-        }
+        } // 입력 끝
 
-        before[0] = -1;
-        before[1] = 0;
-        for(int i = 2; i < N; i++) {
-            if(before[i - 1] == i - 2) {
-                before[i] = i - 2;
-            } else {
-                if(stairs[i - 1] > stairs[i - 2])
-                    before[i] = i - 1;
-                else
-                    before[i] = i - 2;
+        D[0] = stairs[0];
+        D[1] = stairs[0] + stairs[1];
+
+        boolean cont = true;
+        for(int i = 3; i < N; i++) {
+            if(cont) {
+                D[i] = D[i - 2] + stairs[i];
+                cont = false;
+            }
+            else {
+                if(D[i - 2] > D[i - 1])
+                    D[i] = D[i - 2] + stairs[i];
+                else {
+                    D[i] = D[i - 1] + stairs[i];
+                    cont = true;
+                }
             }
         }
 
-        int i = before[N - 1];
-        int sum = stairs[N - 1];
-        while(i >= 0) {
-            sum += stairs[i];
-            i = before[i];
-        }
-
-        System.out.println(sum);
+        System.out.println(D[N - 1]);
     }
 }
