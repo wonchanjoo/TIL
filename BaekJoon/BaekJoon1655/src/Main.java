3df960d139ca64d6c0eff5024a395f089129cece
wonchanjoo <wcj0911@naver.com>
2023-08-08 08:05:39 +0900

    [BaekJoon] 1655 가운데를 말해요 (incomplete)

---
+++ BaekJoon/BaekJoon1655/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static int N;
+    static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(br.readLine());
+        }
+    }
+}
