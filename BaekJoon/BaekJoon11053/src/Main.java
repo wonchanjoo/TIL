b102a3eeaad5dbe1cc1236f087857f372519604a
wonchanjoo <wcj0911@naver.com>
2023-08-15 20:09:33 +0900

    [BaekJoon] 11053 가장 긴 증가하는 부분 수열

---
+++ BaekJoon/BaekJoon11053/src/Main.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[] A;
+    static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        A = new int[N];
+        dp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int max = 1;
+        dp[0] = 1;
+        for (int i = 1; i < N; i++) {
+            dp[i] = 1; // 적어도 길이는 1이다.
+
+            for (int j = 0; j < i; j++) {
+                if (A[j] < A[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
+    }
+}
