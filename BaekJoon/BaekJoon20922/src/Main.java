76aad5410717d8b2978383d0838604f9cac079a7
wonchanjoo <wcj0911@naver.com>
2023-08-16 15:00:56 +0900

    [BaekJoon] 20922 겹치는 건 싫어

--- BaekJoon/BaekJoon20922/src/Main.java
+++ BaekJoon/BaekJoon20922/src/Main.java
@@ -1,35 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+    static int N, K;
+    static int[] a;
+    static int[] count = new int[100_001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] a = new int[N];
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        a = new int[N];
+
         st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] count = new int[100001];
-        int start = 0, end = 0;
-        while(start <= end) {
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        while (end < N) {
             count[a[end]]++;
-            if(count[a[end]] > K) {
-                count[a[start]]--;
+
+            if (count[a[end]] > K) {
+                while (count[a[end]] != K) {
+                    count[a[start++]]--;
+                }
             }
 
-            if(end == a.length - 1) {
-                start++;
-            } else {
-                end++;
-            }
+            max = Math.max(max, end - start + 1);
+            end++;
         }
+
+        System.out.println(max);
     }
 }
