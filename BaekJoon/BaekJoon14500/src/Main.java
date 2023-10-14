4562a350b5f940f7d01360d6655eb8508dbe077b
wonchanjoo <wcj0911@naver.com>
2023-10-14 17:18:02 +0900

    [BaekJoon] 14500 테트로미노

--- BaekJoon/BaekJoon14500/src/Main.java
+++ BaekJoon/BaekJoon14500/src/Main.java
@@ -6,9 +6,12 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int N, M;
-    static int[][] score, tetromino;
+    static int[][] score;
     static boolean[][] visited;
     static int answer = 0;
+
+    static int[] dirR = {-1, 1, 0, 0};
+    static int[] dirC = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,33 +30,45 @@ public class Main {
             }
         }
 
-        initTetromino();
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < M; c++) {
+                if (!visited[r][c]) {
+                    visited[r][c] = true;
+                    dfs(r, c, score[r][c], 1);
+                    visited[r][c] = false;
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 
-    private static void DFS(Point p, int sum) {
-        for (int r = p.r; r < N; r++) {
-            for (int c = (r == p.r) ? p.c : 0; c < M; c++) {
+    private static void dfs(int r, int c, int sum, int count) {
+        if (count == 4) {
+            answer = Math.max(answer, sum);
+            return;
+        }
 
+        for (int i = 0; i < 4; i++) {
+            int nextR = r + dirR[i];
+            int nextC = c + dirC[i];
+
+            if (rangeIn(nextR, nextC) && !visited[nextR][nextC]) {
+
+                if (count == 2) {
+                    visited[nextR][nextC] = true;
+                    dfs(r, c, sum + score[nextR][nextC], count + 1);
+                    visited[nextR][nextC] = false;
+                }
+
+                visited[nextR][nextC] = true;
+                dfs(nextR, nextC, sum + score[nextR][nextC], count + 1);
+                visited[nextR][nextC] = false;
             }
         }
     }
 
-    private static void initTetromino() {
-        tetromino = new int[][]{
-                {0, 0}, {0, 1}, {0, 2}, {0, 3},
-                {0, 0}, {0, 1}, {1, 0}, {1, 1},
-                {0, 0}, {1, 0}, {2, 0}, {2, 1},
-                {0, 0}, {1, 0}, {1, 1}, {2, 1},
-                {0, 0}, {0, 1}, {0, 2}, {1, 1}
-        };
-    }
-
-    static class Point {
-        int r, c;
-
-        public Point(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
+    private static boolean rangeIn(int r, int c) {
+        return (r >= 0) && (c >= 0) && (r < N) && (c < M);
     }
 }
