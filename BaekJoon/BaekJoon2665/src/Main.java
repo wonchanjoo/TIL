327c1f7b061622e999cd09be2d10eabf828dabc7
wonchanjoo <wcj0911@naver.com>
2023-10-29 14:44:50 +0900

    [BaekJoon] 2665 미로만들기

--- BaekJoon/BaekJoon2665/src/Main.java
+++ BaekJoon/BaekJoon2665/src/Main.java
@@ -6,92 +6,64 @@ import java.util.Deque;
 
 public class Main {
 
+    static final int INF = Integer.MAX_VALUE;
     static final int BLACK = 0;
     static final int WHITE = 1;
 
     static int n;
     static int[][] board;
-    static int answer = Integer.MAX_VALUE;
+    static int[][] cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         n = Integer.parseInt(br.readLine());
         board = new int[n][n];
+        cnt = new int[n][n];
 
         for (int r = 0; r < n; r++) {
             String s = br.readLine();
             for (int c = 0; c < n; c++) {
                 board[r][c] = s.charAt(c) - '0';
+                cnt[r][c] = INF;
             }
         }
 
-        if (pass()) {
-            System.out.println(0);
-            return;
-        }
-
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                if (board[r][c] == BLACK) {
-                    board[r][c] = WHITE;
-                    dfs(r, c, 1);
-                    board[r][c] = BLACK;
-                }
-            }
-        }
-
-        System.out.println(answer);
+        solve();
+        System.out.println(cnt[n - 1][n - 1]);
     }
 
-    private static void dfs(int r, int c, int cnt) {
-        if (cnt >= answer) {
-            return;
-        }
+    private static void solve() {
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
+        Deque<Point> deque = new ArrayDeque<>();
 
-        if (pass()) {
-            answer = Math.min(answer, cnt);
-            return;
-        }
+        deque.offer(new Point(0, 0));
+        cnt[0][0] = 0;
 
-        for (int i = r; i < n; i++) {
-            for (int j = (i == r) ? c : 0; j < n; j++) {
-                if (board[i][j] == BLACK) {
-                    board[i][j] = WHITE;
-                    dfs(i, j, cnt + 1);
-                    board[i][j] = BLACK;
-                }
-            }
-        }
-    }
-
-    private static boolean pass() {
-        Deque<Point> queue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[n][n];
-        int[] dirR = {-1, 1, 0, 0};
-        int[] dirC = {0, 0, -1, 1};
-
-        queue.offer(new Point(0, 0));
-        visited[0][0] = true;
-
-        while (!queue.isEmpty()) {
-            Point now = queue.poll();
-            if (now.r == (n - 1) && now.c == (n - 1)) {
-                break;
-            }
+        while (!deque.isEmpty()) {
+            Point now = deque.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nextR = now.r + dirR[i];
-                int nextC = now.c + dirC[i];
+                int nextR = now.r + dr[i];
+                int nextC = now.c + dc[i];
 
-                if (rangeIn(nextR, nextC) && !visited[nextR][nextC]) {
-                    visited[nextR][nextC] = true;
-                    queue.offer(new Point(nextR, nextC));
+                if (!rangeIn(nextR, nextC)) {
+                    continue;
+                }
+
+                int tmp = (board[nextR][nextC] == BLACK) ? cnt[now.r][now.c] + 1 : cnt[now.r][now.c];
+                if (tmp < cnt[nextR][nextC]) {
+                    cnt[nextR][nextC] = tmp;
+
+                    if (board[nextR][nextC] == WHITE) {
+                        deque.offerFirst(new Point(nextR, nextC));
+                    } else {
+                        deque.offerLast(new Point(nextR, nextC));
+                    }
                 }
             }
         }
-
-        return visited[n - 1][n - 1];
     }
 
     private static boolean rangeIn(int r, int c) {
