5d885ff8cd8c32a6aceb167f8fdb12c65dbcb8b3
wonchanjoo <wcj0911@naver.com>
2023-11-10 21:14:13 +0900

    [BaekJoon] 1655 가운데를 말해요

--- BaekJoon/BaekJoon1655/src/Main.java
+++ BaekJoon/BaekJoon1655/src/Main.java
@@ -7,16 +7,36 @@ import java.util.PriorityQueue;
 public class Main {
 
     static int N;
-    static PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-    static PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+    static PriorityQueue<Integer> min = new PriorityQueue<>();
+    static PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
+        max.offer(Integer.parseInt(br.readLine()));
+        sb.append(max.peek()).append('\n');
+        for (int i = 1; i < N; i++) {
             int n = Integer.parseInt(br.readLine());
+
+            if (max.peek() >= n) {
+                max.offer(n);
+            } else {
+                min.offer(n);
+            }
+
+            while (min.size() > max.size()) {
+                max.offer(min.poll());
+            }
+            while (max.size() - min.size() > 1) {
+                min.offer(max.poll());
+            }
+
+            sb.append(max.peek()).append('\n');
         }
+
+        System.out.println(sb);
     }
 }
