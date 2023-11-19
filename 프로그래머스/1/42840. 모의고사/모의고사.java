c90981b904954993b7722379a98b63248fa71f00
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2023-11-20 08:44:30 +0900

    [level 1] Title: 모의고사, Time: 1.52 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] solution(int[] answers) {
+        int[][] supo = {{1, 2, 3, 4, 5},
+                       {2, 1, 2, 3, 2, 4, 2, 5},
+                       {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] index = new int[3];
+        int[] count = new int[3];
+        
+        for(int i = 0; i < answers.length; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(supo[j][index[j]] == answers[i]) {
+                    count[j]++;
+                }
+                index[j] = (index[j] + 1) % supo[j].length;
+            }
+        }
+        
+        // 모두 동점자인 경우
+        if(count[0] == count[1] && count[1] == count[2]) {
+            int[] result = {1, 2, 3};
+            return result;
+        }
+        
+        int max = 0;
+        for(int i = 1; i < 3; i++) {
+            if(count[max] < count[i]) {
+                max = i;
+            } else if(count[max] == count[i]) {
+                int[] result = {(max + 1), (i + 1)};
+                return result;
+            }
+        }
+        int[] result = {(max + 1)};
+        return result;
+    }
+}
