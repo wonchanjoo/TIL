78575f1b129750f61260cce25408cd40eabb8920
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2023-11-19 21:50:09 +0900

    Delete Programmers directory

--- Programmers/Lv.1/평균 구하기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public double solution(int[] arr) {
-        double sum = 0.0;
-        for(int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        return sum / arr.length;
-    }
-}
