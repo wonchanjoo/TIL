d98a058bbc8a30a9d387aefb15703822e6d8f69d
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-03-22 23:15:33 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.cpp
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -4,9 +4,5 @@
 using namespace std;
 
 int solution(int number, int n, int m) {
-    if ((number % n == 0) && (number % m == 0)) {
-        return 1;
-    } else {
-        return 0;
-    }
+    return (number % n == 0 && number % m == 0) ? 1 : 0;
 }
