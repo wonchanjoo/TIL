7bcbfc7a122dd7a889005dd7333390b4284fe707
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-06-03 22:37:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 20.12 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,14 +3,18 @@ import java.util.*;
 class Solution {
     
     List<Integer> list = new ArrayList<>();
+    int min, max;
     
     public String solution(String s) {
         StringTokenizer st = new StringTokenizer(s);
+        min = max = Integer.parseInt(st.nextToken());
+        
         while (st.hasMoreTokens()) {
-            list.add(Integer.parseInt(st.nextToken()));
+            int i = Integer.parseInt(st.nextToken());
+            min = Math.min(min, i);
+            max = Math.max(max, i);
         }
         
-        Collections.sort(list);
-        return list.get(0) + " " + list.get(list.size() - 1);
+        return min + " " + max;
     }
 }
