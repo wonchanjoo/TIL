0181f20f9d3eba117e57d62aee8f7902716e7b57
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-06-05 18:13:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.96 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,38 +3,45 @@ import java.util.*;
 class Solution {
     
     int n, m;
-    int[][] count;
-    int[] dirR = {-1, 1, 0, 0};
-    int[] dirC = {0, 0, -1, 1};
+    int[][] count, maps;
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
     
     public int solution(int[][] maps) {
         n = maps.length;
         m = maps[0].length;
-        
         count = new int[n][m];
+        this.maps = maps;
         
-        Queue<Point> q = new LinkedList<>();
-        q.offer(new Point(0, 0)); // 문제의 (1, 1)을 (0, 0)으로
-        count[0][0] = 1;
-        
-        while(!q.isEmpty()) {
-            Point p = q.poll();
-            
-            for(int i = 0; i < 4; i++) {
-                int nextR = p.r + dirR[i];
-                int nextC = p.c + dirC[i];
-                
-                if(rangeIn(nextR, nextC) && maps[nextR][nextC] == 1 && count[nextR][nextC] == 0) {
-                    q.offer(new Point(nextR, nextC));
-                    count[nextR][nextC] = count[p.r][p.c] + 1;
-                }
-            }
-        }
+        bfs();
         
         if(count[n - 1][m - 1] == 0)
             return -1;
         else
             return count[n - 1][m - 1];
+    }
+    
+    private void bfs() {
+        Deque<Point> queue = new ArrayDeque<>();
+        queue.offer(new Point(0, 0));
+        count[0][0] = 1;
+        
+        while (!queue.isEmpty()) {
+            Point now = queue.poll();
+            if (now.r == n - 1 && now.c == m - 1) {
+                break;
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                int nextR = now.r + dr[i];
+                int nextC = now.c + dc[i];
+                
+                if (rangeIn(nextR, nextC) && maps[nextR][nextC] == 1 && count[nextR][nextC] == 0) {
+                    queue.offer(new Point(nextR, nextC));
+                    count[nextR][nextC] = count[now.r][now.c] + 1;
+                }
+            }
+        }
     }
     
     private boolean rangeIn(int r, int c) {
