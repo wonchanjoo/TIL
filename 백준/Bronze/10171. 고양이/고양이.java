ba00e71ff8663426d9a026bb73d9d01112b3359c
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-06-22 12:17:06 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13940 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append('\n');
+        sb.append(" )  ( ')").append('\n');
+        sb.append("(  /  )").append('\n');
+        sb.append(" \\(__)|").append('\n');
+        System.out.println(sb);
+    }
+}
