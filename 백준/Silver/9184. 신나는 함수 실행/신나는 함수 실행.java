e4d7b24f4838c6e022d2b42bd598ae7987d17c38
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-06-28 14:41:05 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 300 ms, Memory: 24232 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int[][][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        dp = new int[21][21][21];
+        // dp 초기화
+        for (int i = 0; i < 21; i++) {
+            for (int j = 0; j < 21; j++) {
+                for (int k = 0; k < 21; k++) {
+                    dp[i][j][k] = -1;
+                }
+            }
+        }
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+            sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a, b, c)).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    private static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (dp[a][b][c] != -1) {
+            return dp[a][b][c];
+        }
+
+        if (a < b && b < c) {
+            return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c - 1);
+        } else {
+            return dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+    }
+}
