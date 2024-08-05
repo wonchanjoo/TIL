36d275ea6ce82110168bac53734f835bb71532d0
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-08-06 07:53:51 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        
+        for (int i = num_list.length - 1; i >= 0; i--) {
+            answer[answer.length - i - 1] = num_list[i];
+        }
+        
+        return answer;
+    }
+}
