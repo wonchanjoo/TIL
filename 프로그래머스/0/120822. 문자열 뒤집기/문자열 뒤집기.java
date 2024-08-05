22f4cbfe60ba0758ab6c7162c1508cd4d753abf6
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-08-06 07:56:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = my_string.length() - 1; i >= 0; i--) {
+            sb.append(my_string.charAt(i));
+        }
+        
+        return sb.toString();
+    }
+}
