45245d8d0982d55767c3a4d6f8b328090215c3b4
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-10-06 17:50:57 +0900

    [level 0] Title: 홀짝 구분하기, Time: 168.87 ms, Memory: 65 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        if (n % 2 == 0) {
+            System.out.println(n + " is even");
+        } else {
+            System.out.println(n + " is odd");
+        }
+    }
+}
