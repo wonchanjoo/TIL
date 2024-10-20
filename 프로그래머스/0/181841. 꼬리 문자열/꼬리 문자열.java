74fa1fe110145fb28ef15fdcc727513d64989974
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-10-20 13:29:06 +0900

    [level 0] Title: 꼬리 문자열, Time: 0.05 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (String s : str_list) {
+            if (!s.contains(ex)) {
+                sb.append(s);
+            }
+        }
+        
+        return sb.toString();
+    }
+}
