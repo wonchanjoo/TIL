77137adc6ff9a7c2e7627b2c59d241670ccd1da2
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2024-11-05 23:11:19 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int solution(string my_string, string target) {
-    if (my_string.find(target) != -1) {
+    if (my_string.find(target) != string::npos) {
         return 1;
     } else {
         return 0;
