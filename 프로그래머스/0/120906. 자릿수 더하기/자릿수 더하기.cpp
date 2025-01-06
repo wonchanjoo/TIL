84b04ab9ef9e008846f2d231345875385bb4234f
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2025-01-06 13:08:30 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    string s = to_string(n);
+    for (char c : s) {
+        answer += (c - '0');
+    }
+    return answer;
+}
