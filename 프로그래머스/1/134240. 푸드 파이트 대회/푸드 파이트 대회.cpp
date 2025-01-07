321635a4f98858b12c073c1693b943302d7a06dc
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2025-01-08 07:56:56 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.04 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -4,14 +4,11 @@
 using namespace std;
 
 string solution(vector<int> food) {
-    string tmp = "";
-    for (int i = 1; i < food.size(); i++) {
-        tmp += string(food[i] / 2, char(i + '0'));
-    }
+    string answer = "0";
     
-    string answer = tmp + '0';
-    for (int i = tmp.size() - 1; i >= 0; i--) {
-        answer += tmp[i];
+    for (int i = food.size() - 1; i > 0; i--) {
+        string tmp = string(food[i] / 2, char(i + '0'));
+        answer = tmp + answer + tmp;
     }
     
     return answer;
