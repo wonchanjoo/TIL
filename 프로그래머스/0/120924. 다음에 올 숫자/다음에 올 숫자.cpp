41d0e8437ea54a7b976f9ae85f2d6637ed42fb75
원찬주 <92321183+wonchanjoo@users.noreply.github.com>
2025-03-28 12:57:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(vector<int> common) {
+    if ((common[1] - common[0]) == (common[2] - common[1])) {
+        return common.back() + (common[1] - common[0]);
+    } else {
+        return common.back() * (common[1] / common[0]);
+    }
+}
